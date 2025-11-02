da4a52ca36ea551deb1be809d18225c6a33e727b
BigBigBai <baiyingjian@outlook.com>
2025-11-02 13:51:40 -0500

    load products from database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,21 @@
 import ProductList from '@/components/shared/product/product-list';
 import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const Homepage = () => {
+const Homepage = async () => {
   // await delay(2000);
+
+  const latestProducts = await getLatestProducts();
 
   return (
     <div className='space-y-8'>
       <h2 className='h2-bold'>Latest Products</h2>
       <ProductList
         title='Newest Arrivals'
-        data={sampleData.products}
+        // data={sampleData.products}
+        data={latestProducts}
         limit={4}
       />
     </div>
