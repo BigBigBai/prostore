5d49d1435e5bd4d6b6c900f9bd5fcfbb93a23b23
BigBigBai <baiyingjian@outlook.com>
2025-11-02 08:58:33 -0500

    fix hydration error

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -16,6 +16,16 @@ import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
 
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
