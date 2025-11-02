a700d7054cbd71fcc087af400d521a41f2691971
BigBigBai <baiyingjian@outlook.com>
2025-11-02 15:57:44 -0500

    add serverless environment config

---
+++ db/prisma.ts
@@ -0,0 +1,31 @@
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@/lib/generated/prisma/client';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// const pool = new Pool({ connectionString });
+
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
+
+// console.log(prisma);
+// console.log(5);
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,15 +1,18 @@
 'use server';
-import { PrismaClient } from '../generated/prisma/client';
+// import { PrismaClient } from '../generated/prisma/client';
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
+  //   const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
+
+  //   console.log(data);
 
   return convertToPlainObject(data);
 }
