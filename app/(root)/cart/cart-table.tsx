7356b8469737f2b0d20fd1d3b23afe15f804c1e1
BigBigBai <baiyingjian@outlook.com>
2025-11-05 09:40:56 -0500

    create ShadCN Table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -9,7 +9,78 @@ import { Cart } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
+function AddButton({ item }: { item: CartItem }) {
+  // const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      disabled={isPending}
+      variant='outline'
+      type='button'
+      onClick={() =>
+        startTransition(async () => {
+          const res = await addItemToCart(item);
+
+          if (!res.success) {
+            // toast({
+            //   variant: 'destructive',
+            //   description: res.message,
+            // });
+            toast.error(res.message);
+          }
+        })
+      }
+    >
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <Plus className='w-4 h-4' />
+      )}
+    </Button>
+  );
+}
+
+function RemoveButton({ item }: { item: CartItem }) {
+  // const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      disabled={isPending}
+      variant='outline'
+      type='button'
+      onClick={() =>
+        startTransition(async () => {
+          const res = await removeItemFromCart(item.productId);
+
+          if (!res.success) {
+            // toast({
+            //   variant: 'destructive',
+            //   description: res.message,
+            // });
+            toast.error(res.message);
+          }
+        })
+      }
+    >
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <Minus className='w-4 h-4' />
+      )}
+    </Button>
+  );
+}
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -28,6 +99,48 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
           <div className='overflow-x-auto md:col-span-3'></div>
         </div>
       )}
+
+      <div className='grid md:grid-cols-4 md:gap-5'>
+        <div className='overflow-x-auto md:col-span-3'>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Item</TableHead>
+                <TableHead className='text-center'>Quantity</TableHead>
+                <TableHead className='text-right'>Price</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {cart?.items.map((item) => (
+                <TableRow key={item.slug}>
+                  <TableCell>
+                    <Link
+                      href={`/product/${item.slug}`}
+                      className='flex items-center'
+                    >
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={50}
+                        height={50}
+                      ></Image>
+                      <span className='px-2'>{item.name}</span>
+                    </Link>
+                  </TableCell>
+                  <TableCell className='flex-center gap-2'>
+                    <RemoveButton item={item} />
+                    <span>{item.qty}</span>
+                    <AddButton item={item} />
+                  </TableCell>
+                  <TableCell className='text-right'>
+                    ${Number(item.price).toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </>
   );
 };
