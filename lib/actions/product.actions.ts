8e271bc70586434ef052501097cd64f23dbf7470
BigBigBai <baiyingjian@outlook.com>
2025-11-10 09:06:10 -0500

    get Products for admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@
 import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { PAGE_SIZE } from '../constants';
 
 export async function getLatestProducts() {
   //   const prisma = new PrismaClient();
@@ -21,4 +22,30 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category: string;
+}) {
+  // Function body
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
