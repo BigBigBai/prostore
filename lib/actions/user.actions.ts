bfc9e224875d486d7912dd3d3011e7b41195d8ed
BigBigBai <baiyingjian@outlook.com>
2025-11-10 20:43:44 -0500

    get & display users in admin page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,6 +12,7 @@ import { ShippingAddress } from '@/types';
 
 import { paymentMethodSchema } from '../validator';
 import { z } from 'zod';
+import { PAGE_SIZE } from '../constants';
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -179,4 +180,26 @@ export async function updateProfile(user: { name: string; email: string }) {
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
+}
+
+// Get all users
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
