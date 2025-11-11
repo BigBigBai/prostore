5fefd7eb55d7efb60c368263bf05d82948e2ac82
BigBigBai <baiyingjian@outlook.com>
2025-11-10 23:03:19 -0500

    users search

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -15,6 +15,7 @@ import { z } from 'zod';
 import { PAGE_SIZE } from '../constants';
 import { revalidatePath } from 'next/cache';
 import { updateUserSchema } from '../validator';
+import { Prisma } from '@prisma/client';
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -188,11 +189,26 @@ export async function updateProfile(user: { name: string; email: string }) {
 export async function getAllUsers({
   limit = PAGE_SIZE,
   page,
+  query,
 }: {
   limit?: number;
   page: number;
+  query: string;
 }) {
+  const queryFilter: Prisma.UserWhereInput =
+    query && query !== 'all'
+      ? {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.user.findMany({
+    where: {
+      ...queryFilter,
+    },
     orderBy: { createdAt: 'desc' },
     take: limit,
     skip: (page - 1) * limit,
